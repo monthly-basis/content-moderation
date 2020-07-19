2b61c0aafc68c10f9bae05d81476deffc4fd8595
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-07-19 15:59:56 +0000

    Improve spacing

--- src/Model/Service/ContainsBadWords.php
+++ src/Model/Service/ContainsBadWords.php
@@ -11,11 +11,10 @@ class ContainsBadWords
         $this->regularExpressionsOfBadWords = $regularExpressionsOfBadWords;
     }
 
-    public function containsBadWords(
-        string $string
-    ): bool {
+    public function containsBadWords(string $string): bool
+    {
         $patterns = $this->regularExpressionsOfBadWords
-                         ->getRegularExpressionsOfBadWords();
+            ->getRegularExpressionsOfBadWords();
 
         foreach ($patterns as $pattern) {
             if (preg_match($pattern, $string)) {
