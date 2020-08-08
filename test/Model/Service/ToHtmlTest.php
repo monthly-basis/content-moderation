134c6b45ba53efe1271300c15fbc6103086a5ac0
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-08-08 02:40:14 +0000

    Write test to ensure proper HTML is returned for URLs

--- test/Model/Service/ToHtmlTest.php
+++ test/Model/Service/ToHtmlTest.php
@@ -12,17 +12,20 @@ class ToHtmlTest extends TestCase
         $this->replaceBadWordsServiceMock = $this->createMock(
             ContentModerationService\ReplaceBadWords::class
         );
+        $this->escapeServiceMock = $this->createMock(
+            StringService\Escape::class
+        );
 
         $this->toHtmlService = new ContentModerationService\ToHtml(
             $this->replaceBadWordsServiceMock,
-            new StringService\Escape()
+            $this->escapeServiceMock
         );
     }
 
     public function testToHtml()
     {
-        $this->replaceBadWordsServiceMock
-             ->method('replaceBadWords')
+        $this->escapeServiceMock
+             ->method('escape')
              ->willReturn('hello world');
         $string = 'hello world';
         $this->assertSame(
@@ -33,6 +36,19 @@ class ToHtmlTest extends TestCase
         $string = "\n\n   hello world\t\t   \t\n\n";
         $this->assertSame(
             'hello world',
+            $this->toHtmlService->toHtml($string)
+        );
+    }
+
+    public function test_toHtml_stringWithUrls_correctHtml()
+    {
+        $string = 'https://www.jiskha.com https://www.google.com';
+        $this->escapeServiceMock
+             ->method('escape')
+             ->willReturn($string);
+
+        $this->assertSame(
+            '<a href="https://www.jiskha.com">https://www.jiskha.com</a> <a href="https://www.google.com" target="_blank" rel="nofollow external noopener">https://www.google.com</a>',
             $this->toHtmlService->toHtml($string)
         );
     }
